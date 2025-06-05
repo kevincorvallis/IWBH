62c80ae4a6c65f81194b3048033bfd02261eb7b4
Kevin Lee <kevincorvallis@gmail.com>
2025-06-05 13:27:10 -0700

    Initial Commit

---
+++ IWBH/IWBHApp.swift
@@ -0,0 +1,17 @@
+//
+//  IWBHApp.swift
+//  IWBH
+//
+//  Created by Kevin Lee on 6/5/25.
+//
+
+import SwiftUI
+
+@main
+struct IWBHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
